080c56e751d1efdba56a21dcc120741cbda38cc0
Shashank T K <shashanktk510@gmail.com>
2026-07-31 20:42:19 +0530

    Time: 29 ms (4.70%) | Memory: 46.2 MB (6.18%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0) return false;
+        int org=x;
+        int res=0;
+        while(x!=0){
+            int rem = x%10;
+            res=(res*10)+rem;
+            x/=10;
+        }
+        System.out.print(res);
+        return org==res;
+    }
+}
